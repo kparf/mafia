275a74ced9ea82176a2cf2ce1e603d586c14990e
Kiryl Parfiankou <Kiryl.Parfiankou.dev@gmail.com>
2017-11-16 20:25:45 +0300

    Style updates.

--- routes/middleware.js
+++ routes/middleware.js
@@ -18,16 +18,16 @@ var _ = require("lodash");
 	or replace it with your own templates / logic.
 */
 exports.initLocals = function (req, res, next) {
-	res.locals.navLinks = [
-		{ label : "Клу", key : "home", href : "/" },
-		{ label : "Новости", key : "blog", href : "/blog" },
-		{ label : "Где Играть", key : "location", href : "/location" },
-		{ label : "Рейтинг", key : "rating", href : "/rating" },
-		{ label : "Фото Галерея", key : "gallery", href : "/gallery" },
-		{ label : "Контакты", key : "contact", href : "/contact" }
-	];
-	res.locals.user = req.user;
-	next();
+    res.locals.navLinks = [
+        { label : "Клуб", key : "home", href : "/" },
+        { label : "Новости", key : "blog", href : "/blog" },
+        { label : "Где Играть", key : "location", href : "/location" },
+        { label : "Рейтинг", key : "rating", href : "/rating" },
+        { label : "Фото Галерея", key : "gallery", href : "/gallery" },
+        { label : "Контакты", key : "contact", href : "/contact" }
+    ];
+    res.locals.user = req.user;
+    next();
 };
 
 
@@ -35,14 +35,14 @@ exports.initLocals = function (req, res, next) {
 	Fetches and clears the flashMessages before a view is rendered
 */
 exports.flashMessages = function (req, res, next) {
-	var flashMessages = {
-		info : req.flash("info"),
-		success : req.flash("success"),
-		warning : req.flash("warning"),
-		error : req.flash("error")
-	};
-	res.locals.messages = _.some(flashMessages, function (msgs) { return msgs.length; }) ? flashMessages : false;
-	next();
+    var flashMessages = {
+        info : req.flash("info"),
+        success : req.flash("success"),
+        warning : req.flash("warning"),
+        error : req.flash("error")
+    };
+    res.locals.messages = _.some(flashMessages, function (msgs) { return msgs.length; }) ? flashMessages : false;
+    next();
 };
 
 
@@ -50,10 +50,10 @@ exports.flashMessages = function (req, res, next) {
 	Prevents people from accessing protected pages when they're not signed in
  */
 exports.requireUser = function (req, res, next) {
-	if (!req.user) {
-		req.flash("error", "Please sign in to access this page.");
-		res.redirect("/keystone/signin");
-	} else {
-		next();
-	}
+    if (!req.user) {
+        req.flash("error", "Please sign in to access this page.");
+        res.redirect("/keystone/signin");
+    } else {
+        next();
+    }
 };
