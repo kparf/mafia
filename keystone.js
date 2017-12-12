e83d48da721b231d4d10c5ddebc361effd6d8c81
Kiryl Parfiankou <Kiryl.Parfiankou.dev@gmail.com>
2017-12-13 02:29:55 +0300

    Fix winPercentage column

--- keystone.js
+++ keystone.js
@@ -5,35 +5,42 @@ require("dotenv").config();
 // Require keystone
 var keystone = require("keystone");
 var handlebars = require("express-handlebars");
+var helpers = require("handlebars-helpers");
+
+const numberHelpers = helpers.number();
+const customHelpers = new require("./templates/views/helpers")()
 
 // Initialise Keystone with your projects configuration.
 // See http://keystonejs.com/guide/config for available options
 // and documentation.
 
 keystone.init({
-	"name" : "parf.by",
-	"brand" : "parf.by",
+    "name" : "parf.by",
+    "brand" : "parf.by",
 
-	"less" : "public",
-	"static" : "public",
-	"favicon" : "public/favicon.ico",
-	"views" : "templates/views",
-	"view engine" : ".hbs",
+    "less" : "public",
+    "static" : "public",
+    "favicon" : "public/favicon.ico",
+    "views" : "templates/views",
+    "view engine" : ".hbs",
 
-	"custom engine" : handlebars.create({
-		layoutsDir : "templates/views/layouts",
-		partialsDir : "templates/views/partials",
-		defaultLayout : "default",
-		helpers : new require("./templates/views/helpers")(),
-		extname : ".hbs"
-	}).engine,
+    "custom engine" : handlebars.create({
+        layoutsDir : "templates/views/layouts",
+        partialsDir : "templates/views/partials",
+        defaultLayout : "default",
+        helpers : {
+            ...numberHelpers,
+            ...customHelpers
+        },
+        extname : ".hbs"
+    }).engine,
 
-	"emails" : "templates/emails",
+    "emails" : "templates/emails",
 
-	"auto update" : true,
-	"session" : true,
-	"auth" : true,
-	"user model" : "User"
+    "auto update" : true,
+    "session" : true,
+    "auth" : true,
+    "user model" : "User"
 });
 
 // Load your project's Models
@@ -43,10 +50,10 @@ keystone.import("models");
 // bundled templates and layouts. Any runtime locals (that should be set uniquely
 // for each request) should be added to ./routes/middleware.js
 keystone.set("locals", {
-	_ : require("lodash"),
-	env : keystone.get("env"),
-	utils : keystone.utils,
-	editable : keystone.content.editable
+    _ : require("lodash"),
+    env : keystone.get("env"),
+    utils : keystone.utils,
+    editable : keystone.content.editable
 });
 
 // Load your project's Routes
@@ -55,23 +62,23 @@ keystone.set("routes", require("./routes"));
 
 // Configure the navigation bar in Keystone's Admin UI
 keystone.set("nav", {
-	posts : ["posts", "post-categories"],
-	galleries : "galleries",
-	enquiries : "enquiries",
-	users : "users"
+    posts : ["posts", "post-categories"],
+    galleries : "galleries",
+    enquiries : "enquiries",
+    users : "users"
 });
 
 // Start Keystone to connect to your database and initialise the web server
 
 
 if (!process.env.MAILGUN_API_KEY || !process.env.MAILGUN_DOMAIN) {
-	console.log("----------------------------------------"
-	+ "\nWARNING: MISSING MAILGUN CREDENTIALS"
-	+ "\n----------------------------------------"
-	+ "\nYou have opted into email sending but have not provided"
-	+ "\nmailgun credentials. Attempts to send will fail."
-	+ "\n\nCreate a mailgun account and add the credentials to the .env file to"
-	+ "\nset up your mailgun integration");
+    console.log("----------------------------------------"
+    + "\nWARNING: MISSING MAILGUN CREDENTIALS"
+    + "\n----------------------------------------"
+    + "\nYou have opted into email sending but have not provided"
+    + "\nmailgun credentials. Attempts to send will fail."
+    + "\n\nCreate a mailgun account and add the credentials to the .env file to"
+    + "\nset up your mailgun integration");
 }
 
 
